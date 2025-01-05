4840c260e787c95646e3da9ae4090edfbee9ef05
Ivan Shakhman <shakhmanivan@gmail.com>
2025-01-05 19:28:36 +0200

    add logging for repo_extractor.py

--- repo_extractor.py
+++ repo_extractor.py
@@ -4,6 +4,10 @@ import json
 import requests
 from dotenv import load_dotenv
 from utils import url_extractor
+import logging
+
+
+logger = logging.getLogger("uvicorn")
 
 load_dotenv()
 
@@ -16,21 +20,35 @@ if GIT_KEY:
 
 def get_all_branches(owner: str, repo: str):
     """return list of branch names"""
+    logger.info("Get all branches from repository")
+
     url = f"https://api.github.com/repos/{owner}/{repo}/branches"
     response = requests.get(url, headers=HEADER)
-    response.raise_for_status()
+
+    if response.raise_for_status():
+        logger.error(f"ERROR! {response.raise_for_status()}")
+
+    logger.info("Success!")
     return [branch["name"] for branch in response.json()]
 
 
 def download_branch_content(branch_name: str, owner: str, repo: str):
+    logger.info(f"Downloading content of branch: {branch_name}")
+
     url = f"https://api.github.com/repos/{owner}/{repo}/contents"
     params = {"ref": branch_name}
     response = requests.get(url, headers=HEADER, params=params)
-    response.raise_for_status()
+
+    if response.raise_for_status():
+        logger.error(f"ERROR! {response.raise_for_status()}")
+
+    logger.info("Success.")
     return response.json()
 
 
 def save_branch_content(branch_name, content, parent_dir=""):
+    logger.info(f"Start downloading all files from branch {branch_name}")
+
     branch_dir = os.path.join("downloaded_repo", branch_name, parent_dir)
     os.makedirs(branch_dir, exist_ok=True)
 
@@ -42,14 +60,16 @@ def save_branch_content(branch_name, content, parent_dir=""):
                 file_content = base64.b64decode(item["content"])
                 with open(file_path, "wb") as f:
                     f.write(file_content)
+                    logger.info(f"{item["name"]} downloaded successfully.")
 
             elif "download_url" in item:
                 file_url = item["download_url"]
                 file_path = os.path.join(branch_dir, item["name"])
                 download_file(file_url, file_path)
+                logger.info(f"{item["name"]} downloaded successfully.")
 
             else:
-                print(f"Skipping item without 'content' or 'download_url' key: {item}")
+                logger.warning(f"Skipping item without 'content' or 'download_url' key: {item}")
 
         elif item["type"] == "dir":
             dir_content_url = item["url"]
@@ -57,25 +77,34 @@ def save_branch_content(branch_name, content, parent_dir=""):
             response.raise_for_status()
             dir_content = response.json()
             save_branch_content(branch_name, dir_content, os.path.join(parent_dir, item["name"]))
+            logger.info(f"{item["name"]} downloaded successfully.")
         else:
-            print(f"Skipping unsupported item type: {item['type']}")
+            logger.warning(f"Skipping unsupported item type: {item['type']}")
 
 
 def download_file(url, save_path):
+    logger.info("start downloading file...")
+
     try:
         response = requests.get(url, headers=HEADER)
         response.raise_for_status()
         with open(save_path, "wb") as f:
             f.write(response.content)
-        print(f"Downloaded file saved to {save_path}")
+        logger.info(f"Success. Downloaded file saved to {save_path}")
+
     except requests.exceptions.RequestException as e:
-        print(f"Error downloading {url}: {e}")
+        logger.error(f"Error downloading {url}: {e}")
 
 
 def download_all_branches(url: str):
     owner, repo = url_extractor(url)
     branches = get_all_branches(owner=owner, repo=repo)
+
     for branch in branches:
-        print(f"Downloading content of branch: {branch}")
-        content = download_branch_content(branch, owner, repo)
-        save_branch_content(branch, content)
+
+        try:
+            content = download_branch_content(branch, owner, repo)
+            save_branch_content(branch, content)
+
+        except Exception as e:
+            logger.error(f"ERROR: {e}")
--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
+import logging
+
+
+logger = logging.getLogger("uvicorn")
+
+
 def url_extractor(url: str) -> tuple[str, str]:
     """Extract name of account and name of repository and return it in tuple"""
-
+    logger.info("Extract owner, repo from url")
     res = url.split("/")
     repo = res[-1].split(".")[0]
     account = res[-2]
@@ -9,5 +15,6 @@ def url_extractor(url: str) -> tuple[str, str]:
 
 def is_valid_file(file_name):
     """check is a file has a valid format"""
+    logger.info(f"check is correct format of {file_name}")
     valid_extensions = [".py", ".md", ".txt", ".rst", ".yml", ".json"]
     return any(file_name.endswith(ext) for ext in valid_extensions)
